7690dc411651bf18781a9c018e7be4fa7a5c7867
Barnabas A Nsoh <barnabas.ayinloya@meltwater.org>
2014-10-29 18:45:11 +0000

    conflict resolved in problem.rb

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,7 +1,4 @@
 class Problem < ActiveRecord::Base
-<<<<<<< HEAD
-  validates :title, :text, presence: true
-=======
-  has_many :solutions
->>>>>>> edf0a7717050cceef77eb6455951fa618438e29f
+ validates :title, :text, presence: true
+ has_many :solutions
 end
