94ca2b42cd39da381bdafeee4ecd9725f7889f03
Esther Olatunde <olatundeesther@gmail.com>
2014-11-01 17:46:48 +0000

    problem and solution controllers

---
+++ app/controllers/problems_controller.rb
@@ -0,0 +1,21 @@
+class ProblemsController < ApplicationController
+  def index
+    
+  end
+
+  def new
+    
+  end
+
+  def list
+    
+  end
+
+  def show
+    
+  end
+
+  def confirm
+    
+  end
+end
