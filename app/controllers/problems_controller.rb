c766ff9365308f96c27c5f8cd1e20c304ae19a08
Kennedy <kennedy.anyinatoe@meltwater.org>
2014-11-02 11:41:07 +0000

    The problem data insertion has been created. About to add the picture upload function

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,21 +1,33 @@
 class ProblemsController < ApplicationController
   def index
-    
+    @problem = Problem.all
   end
 
   def new
-    
+    @problem = Problem.new
+  end
+
+  def create
+    @problem = Problem.new(problem_params)
+
+@problem.save
+redirect_to @problem
   end
 
   def list
-    
+    @problem = Problem.all
   end
 
   def show
-    
+    @problem = Problem.find(params[:id])
   end
 
   def confirm
     
   end
 end
+private
+def problem_params
+  params.require(:problem). permit(:title, :text, :screenshot)
+end
+
