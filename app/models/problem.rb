97226a6582cea01bed3d4bc68762bd459f66c29b
kwame oga yeboah <kwame.yeboah@meltwater.org>
2014-11-02 19:31:11 +0000

    pulling solutions and pushing some templates

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,7 +1,6 @@
 class Problem < ActiveRecord::Base
+
   validates :title, :text, presence: true,length: { minimum: 10 }
  has_many :solutions
- # accepts_nested_attributes_for :solutions
-# mount_uploader :picture, PictureUploader
-
+accepts_nested_attributes_for :solutions
 end
---
+++ app/models/remove_screenshot_from_solutions.rb
@@ -0,0 +1,2 @@
+class RemoveScreenshotFromSolutions < ActiveRecord::Base
+end
