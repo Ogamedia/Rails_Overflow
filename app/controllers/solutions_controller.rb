6306ce6479d6b8e813eb1a6629fc46faff4221db
kwame oga yeboah <kwame.yeboah@meltwater.org>
2014-11-02 22:40:57 +0000

    solution ui not done

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -2,12 +2,11 @@ class SolutionsController < ApplicationController
   def create
     @problem = Problem.find(params[:problem_id])
     @solution = @problem.solutions.create(solution_params)
-    binding.pry
     redirect_to problem_path(@problem)
   end
 
   private
   def solution_params
-    params.require(:solution).permit(:text,:screenshot,:upvote,:downvote,:problem_id)
+    params.require(:solution).permit(:text,:upvote,:downvote,:problem_id)
   end
 end
